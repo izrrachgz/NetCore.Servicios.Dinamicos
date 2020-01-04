39fa3ccb5e4f2736793d1d9ff7f47d1ee1a32381
israel chavez <izrra.ch@icloud.com>
2020-01-03 22:36:18 -0700

    #movido modelos y enumerados faltantes a sus secciones correspondientes

---
+++ Datos/Modelos/ColumnaDeTabla.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Datos.Modelos
+{
+  /// <summary>
+  /// Provee un modelo de datos para representar una celda de una tabla
+  /// </summary>
+  public class ColumnaDeTabla
+  {
+    /// <summary>
+    /// Indice de la columna
+    /// </summary>
+    public int Indice { get; }
+
+    /// <summary>
+    /// Nombre de la columna
+    /// </summary>
+    public string Nombre { get; }
+
+    /// <summary>
+    /// Valor de la celda
+    /// </summary>
+    public object Celda { get; }
+
+    public ColumnaDeTabla() { }
+
+    public ColumnaDeTabla(int indice, string nombre, object valor)
+    {
+      Indice = indice;
+      Nombre = nombre;
+      Celda = valor == DBNull.Value ? null : valor;
+    }
+  }
+}
--- Datos/Modelos/Union.cs
+++ Datos/Modelos/Union.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Datos.Enumerados;
 
 namespace Datos.Modelos
 {
@@ -34,81 +35,6 @@ namespace Datos.Modelos
       Uniones = uniones;
       Tipo = tipo;
       Seleccion = seleccion;
-    }
-  }
-
-  /// <summary>
-  /// Tipo de union
-  /// </summary>
-  public enum TipoUnion
-  {
-    /// <summary>
-    /// Unión interna (inner join)
-    /// </summary>
-    Interna = 0,
-
-    /// <summary>
-    /// Unión a la izqueirda (left join)
-    /// </summary>
-    Izquierda = 1,
-
-    /// <summary>
-    /// Unión a la derecha (right join)
-    /// </summary>
-    Derecha = 2,
-  }
-
-  /// <summary>
-  /// Provee un modelo de datos para representar una celda de una tabla
-  /// </summary>
-  public class ColumnaDeTabla
-  {
-    /// <summary>
-    /// Indice de la columna
-    /// </summary>
-    public int Indice { get; }
-
-    /// <summary>
-    /// Nombre de la columna
-    /// </summary>
-    public string Nombre { get; }
-
-    /// <summary>
-    /// Valor de la celda
-    /// </summary>
-    public object Celda { get; }
-
-    public ColumnaDeTabla() { }
-
-    public ColumnaDeTabla(int indice, string nombre, object valor)
-    {
-      Indice = indice;
-      Nombre = nombre;
-      Celda = valor == DBNull.Value ? null : valor;
-    }
-  }
-
-  /// <summary>
-  /// Provee un modelo de datos para representar la fila de una tabla
-  /// </summary>
-  public class FilaDeTabla
-  {
-    /// <summary>
-    /// Indice de la fila
-    /// </summary>
-    public int Indice { get; }
-
-    /// <summary>
-    /// Columnas de la fila
-    /// </summary>
-    public List<ColumnaDeTabla> Columnas { get; set; }
-
-    public FilaDeTabla() { }
-
-    public FilaDeTabla(int indice, List<ColumnaDeTabla> columnas)
-    {
-      Indice = indice;
-      Columnas = columnas;
     }
   }
 }
