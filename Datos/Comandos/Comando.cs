b9b18bb1bffe4bf6ec24826a133e099890119628
israel chavez <izrra.ch@icloud.com>
2020-01-03 22:37:32 -0700

    +using de nuevos nombres de espacio

--- Datos/Comandos/Comando.cs
+++ Datos/Comandos/Comando.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datos.Enumerados;
 using Datos.Extensiones;
 using Datos.Modelos;
 using Datos.Utilidades;
