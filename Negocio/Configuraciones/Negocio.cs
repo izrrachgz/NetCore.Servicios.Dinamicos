9b1dccfd9a662b1ab609673b364cb56d74546819
israel chavez <izrra.ch@icloud.com>
2020-01-08 23:07:46 -0700

    #nombre asociativo de configuracion -cofiguracion de negocio no necesaria

---
+++ Negocio/Configuraciones/Negocio.cs
@@ -0,0 +1,13 @@
+﻿using Datos.Configuraciones;
+
+namespace Negocio.Configuraciones
+{
+  /// <summary>
+  /// Provee un modelo de datos para representar
+  /// la configuracion que se asocia al contexto
+  /// del negocio
+  /// </summary>
+  internal sealed class ConfiguracionNegocio : ConfiguracionBase
+  {
+  }
+}
