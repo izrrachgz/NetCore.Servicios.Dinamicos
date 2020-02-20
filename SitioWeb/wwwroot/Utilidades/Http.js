aefaf681bc36ba3be6c092ae54270d29d965d0d6
israel chavez <izrra.ch@icloud.com>
2020-02-19 22:04:48 -0700

    +utilidades js  +primitivo.js  +http.js

---
+++ SitioWeb/wwwroot/Utilidades/Http.js
@@ -0,0 +1,67 @@
+﻿var SolicitudHttp = function () {
+
+  /**
+   * @description Efectua una solicitud Http via Fetch de tipo POST
+   * @param {any} urlBase Url base del recurso
+   * @param {any} metodo Metodo para acceder al recurso
+   * @param {any} parametros Parametros que se deben incluir en la solicitud codificados como Json
+   * @return {any} Promesa de solicitud
+   */
+  this.Post = function (urlBase, metodo, parametros) {
+    //Si los parametros no son validos se omite la solicitud.
+    if (ObjetoInvalido(urlBase) || ObjetoInvalido(metodo) || ObjetoInvalido(parametros)) {
+      return undefined;
+    }
+    return fetch(urlBase + metodo,
+      {
+        method: "POST",
+        body: JSON.stringify(parametros),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+  };
+
+  /**
+   * @description Efectua una solicitud Http via Fetch de tipo GET
+   * @param {any} urlBase Url base del recurso
+   * @param {any} metodo Metodo para acceder al recurso
+   * @param {any} parametros Parametros que se deben incluir en la solicitud codificados como Url
+   * @return {any} Promesa de solicitud
+   */
+  this.Get = function (urlBase, metodo, parametros) {
+    //Si los parametros no son validos se omite la solicitud
+    if (ObjetoInvalido(urlBase) || ObjetoInvalido(metodo)) {
+      return undefined;
+    }
+    if (!ObjetoInvalido(parametros)) {
+      parametros = parametros.substr(0, 1) === "?" ? parametros : "?" + parametros;
+    }
+    return fetch(urlBase + metodo + parametros,
+      {
+        method: "GET"
+      });
+  };
+
+  /**
+   * @description Efectua una solicitud Http via Fetch de tipo DELETE
+   * @param {any} urlBase Url base del recurso
+   * @param {any} metodo Metodo para acceder al recurso
+   * @param {any} parametros Parametros que se deben incluir en la solicitud codificados como Url
+   * @return {any} Promesa de solicitud
+   */
+  this.Delete = function (urlBase, metodo, parametros) {
+    //Si los parametros no son validos se omite la solicitud
+    if (ObjetoInvalido(urlBase) || ObjetoInvalido(metodo)) {
+      return undefined;
+    }
+    if (!ObjetoInvalido(parametros)) {
+      parametros = parametros.substr(0, 1) === "?" ? parametros : "?" + parametros;
+    }
+    return fetch(urlBase + metodo + parametros,
+      {
+        method: "delete"
+      });
+  };
+
+};
---
+++ SitioWeb/wwwroot/Utilidades/Primitivo.js
@@ -0,0 +1,26 @@
+﻿/**
+ * @description Verifica que el objeto dado es invalido para su uso
+ * @param {any} o Objeto a evaluar
+ * @return {boolean} Verdadero o falso
+ */
+function ObjetoInvalido(o) {
+  //Si no esta definido es invalido para su uso
+  if (typeof o === "undefined") return true;
+  //Si no es una cadena y esta vacia es invalido para su uso
+  if (typeof o === "string" && o.trim().length === 0) return true;
+  //Si es nulo es invalido para su uso
+  if (o === null) return true;
+  return false;
+}
+
+/**
+ * @description Verifica si el objeto dado es un numero
+ * @param {any} o Objeto a evaluar
+ * @return {boolean} Verdadero o falso
+ */
+function EsNumero(o) {
+  if (typeof o === "undefined") return false;
+  if (o === null) return false;
+  if (typeof o === "number") return true;
+  return false;
+}
