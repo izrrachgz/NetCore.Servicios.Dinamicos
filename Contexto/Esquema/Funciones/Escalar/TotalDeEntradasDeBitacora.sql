f0c794a8f2d7c72b60b7adc53ec5f5e36e130c65
israel chavez <izrra.ch@icloud.com>
2020-03-18 22:48:36 -0700

    +ejemplo de definicion para procedimiento almacenado y funcion escalar

---
+++ Contexto/Esquema/Funciones/Escalar/TotalDeEntradasDeBitacora.sql
@@ -0,0 +1,63 @@
+-- =============================================
+-- Author:		Israel Ch
+-- Create date: 18/03/2020
+-- Description:	Permite obtener el total de entradas de bitacora registradas dentro del rango de tiempo especificado
+-- =============================================
+CREATE FUNCTION [dbo].[TotalDeEntradasDeBitacora]
+(
+	--Rango de inicio del momento de registro para incluir en la busqueda
+	@Inicio Date null,
+	--Rango de fin del momento de registro para incluir en la busqueda
+	@Fin Date null
+)
+RETURNS bigint
+AS
+BEGIN
+	--Cantidad total de registros de bitacora
+	DECLARE @Total bigint = 0;
+	
+	--Momento de inicio en el que se ha registrado la entrada de bitacora
+	DECLARE @FechaInicio Date = @Inicio;
+	
+	--Momento de fin en el que se ha registrado la entrada de bitacora
+	DECLARE @FechaFin Date = @Fin;
+
+	--Tomar en cuenta las 2 fechas
+	IF @FechaInicio IS NOT NULL AND @FechaFin IS NOT NULL
+	BEGIN
+		SET @Total = (
+			SELECT 
+				COUNT(*) AS Total
+			FROM Bitacora B
+			WHERE B.Creado >= @FechaInicio 
+				AND B.Creado <= @FechaFin
+				AND B.Eliminado IS NULL
+		);
+	END
+
+	--Tomar en cuenta solo la fecha de inicio
+	IF @FechaInicio IS NOT NULL AND @FechaFin IS NULL
+	BEGIN
+		SET @Total = (
+			SELECT 
+				COUNT(*) AS Total
+			FROM Bitacora B
+			WHERE B.Creado >= @FechaInicio 					
+				AND B.Eliminado IS NULL
+		);
+	END
+
+	--Tomar en cuenta solo la fecha de fin
+	IF @FechaInicio IS NULL AND @FechaFin IS NOT NULL
+	BEGIN
+		SET @Total = (
+			SELECT 
+				COUNT(*) AS Total
+			FROM Bitacora B
+			WHERE B.Creado <= @FechaFin 					
+				AND B.Eliminado IS NULL
+		);
+	END
+
+	RETURN @Total;
+END
---
+++ Contexto/Esquema/Procedimientos/EliminarEntradasDeBitacora.sql
@@ -0,0 +1,81 @@
+-- =============================================
+-- Author:		Israel Ch.
+-- Create date: 18/03/2020
+-- Description:	Permite marcar como eliminados los registros de bitacora de un rango de tiempo especificado
+-- =============================================
+CREATE PROCEDURE [dbo].[EliminarEntradasDeBitacora]
+	@Inicio Date,
+	@Fin Date
+AS
+BEGIN	
+	--Prevenir la seleccion multiple de resultados
+	SET NOCOUNT ON;
+
+    --Momento en el que se ha registrado la entrada y deberan eliminarse
+	DECLARE @FechaInicio Date = @Inicio;
+	
+	--Ultimo momento en el que se ha registrado la entrada y debera eliminarse
+	DECLARE @FechaFin Date = @Fin;	
+	
+	--Registro de resultados
+	DECLARE @Resultados TABLE(
+		--Indica si el resultado de la operacion en curso es incorrecto o no (0,1)
+		Correcto bit,
+		--Proporciona el contexto del resultado de la operacion
+		Mensaje varchar(512) null
+	);
+	
+	--Iniciar transaccion de operaciones
+	BEGIN TRANSACTION T0
+	BEGIN TRY
+		--Marcar como eliminado todos los registros de bitacora
+		UPDATE B
+			SET B.Eliminado = GETDATE()
+		FROM Bitacora B
+		WHERE B.Creado >= @FechaInicio
+			 AND B.Creado <= @FechaFin
+			 AND B.Eliminado IS NULL;
+
+		--Marcar como eliminado todos los detalles de bitacora
+		UPDATE D
+			SET D.Eliminado = GETDATE()
+		FROM BitacoraDetalle D
+		WHERE D.Creado >= @FechaInicio
+			 AND D.Creado <= @FechaFin
+			 AND D.Eliminado IS NULL;
+			 		
+		--Terminar la transaccion
+		COMMIT TRANSACTION T0;	
+		
+		--Insertar dentro de los resultados todo correcto
+		INSERT INTO @Resultados (			
+			Correcto,			
+			Mensaje
+		)
+		VALUES (
+			1,
+			N'Se han marcado todos los registros dentro del rango especificado como eliminados'
+		)
+	END TRY
+	BEGIN CATCH
+		--Revertir todos los cambios
+		ROLLBACK TRANSACTION T0;
+		
+		--Insertar dentro de los resultados los errores
+		INSERT INTO @Resultados (
+			Correcto,
+			Mensaje
+		)
+		VALUES (
+			0,
+			ERROR_MESSAGE()
+		)
+	END CATCH		
+
+	--Seleccionar todos los resultados
+	SELECT
+		Correcto,
+		Mensaje
+	FROM 
+		@Resultados;
+END
