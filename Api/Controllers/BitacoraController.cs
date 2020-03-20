ee3d6edeec29bbedd9d26073e1892682d6e1c16d
israel chavez <izrra.ch@icloud.com>
2020-03-19 21:10:28 -0700

    #Controlador api de bitacora  +metodos equivalentes de control de proveedor de datos #Proveedor de datos de bitacora reducido exposicion de metodos de acceso a repositorio de datos

--- Api/Controllers/BitacoraController.cs
+++ Api/Controllers/BitacoraController.cs
@@ -7,31 +7,52 @@ using Negocio.ProveedoresDeDatos;
 namespace Api.Controllers
 {
   /// <summary>
-  /// Provee un mecanismo de acceso a los datos de las entradas
-  /// de log
+  /// Provee un mecanismo para exponer los puntos de entrada
+  /// de negocio de Bitacora
   /// </summary>
   [ApiController, Route("Api/[controller]")]
   public class BitacoraController : ControllerBase
   {
     /// <summary>
-    /// Referencia de acceso al repositorio de datos
-    /// de la entidad EntradaLog
+    /// Referencia de acceso hacia los datos de Bitacora via Negocio
     /// </summary>
     private ProveedorBitacora ProveedorBitacora { get; }
 
+    /// <summary>
+    /// Crea una nueva instancia del servicio de Bitacora
+    /// </summary>
     public BitacoraController()
     {
       ProveedorBitacora = new ProveedorBitacora();
     }
 
     /// <summary>
-    /// Permite obtener todas las entradas de log utilizando
+    /// Permite obtener todos los registros de Bitacora utilizando
     /// un indice de pagina
     /// </summary>
     /// <param name="solicitud">Solicitud de pagina</param>
-    /// <returns></returns>
-    [HttpPost, Route(@"Paginado")]
+    /// <returns>RespuestaColeccion de Bitacora</returns>
+    [HttpPost, Route(@"ObtenerPorPagina")]
     public async Task<RespuestaColeccion<Bitacora>> Obtener(SolicitudPagina solicitud)
       => await ProveedorBitacora.Obtener(new Paginado(solicitud));
+
+    /// <summary>
+    /// Permite obtener un registro de Bitacora mediante su identificador primario
+    /// </summary>
+    /// <param name="id">Identificador primario</param>
+    /// <returns>RespuestaModelo de Bitacora</returns>
+    [HttpPost, Route(@"ObtenerPorId/{id}")]
+    public async Task<RespuestaModelo<Bitacora>> ObtenerPorId([FromQuery] long id)
+      => await ProveedorBitacora.ObtenerPorId(id);
+
+    /// <summary>
+    /// Permite guardar un registro de bitacora ya sea nuevo o modificacion
+    /// de los valores del mismo
+    /// </summary>
+    /// <param name="modelo">Bitacora</param>
+    /// <returns>RespuestaBasica</returns>
+    [HttpPost, Route(@"GuardarBitacora")]
+    public async Task<RespuestaBasica> GuardarBitacora([FromBody] Bitacora modelo)
+      => await ProveedorBitacora.GuardarBitacora(modelo);
   }
 }
--- Negocio/ProveedoresDeDatos/Bitacora.cs
+++ Negocio/ProveedoresDeDatos/Bitacora.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Contexto.Entidades;
 using Contexto.ProveedoresDeDatos;
 using Utilidades.Modelos;
@@ -27,36 +26,7 @@ namespace Negocio.ProveedoresDeDatos
     /// <param name="id">Identificador Primario de Bitacora</param>
     /// <returns>Elemento de Bitacora</returns>
     public async Task<RespuestaModelo<Bitacora>> ObtenerPorId(long id)
-      => await base.Obtener(id);
-
-    /// <summary>
-    /// Devuelve todos los registros de Bitacora dentro de un modelo de datos paginado
-    /// </summary>
-    /// <param name="paginado">Solicitud de Pagina</param>
-    /// <param name="condicion">Condiciones de busqueda</param>
-    /// <returns>Coleccion de elementos de Bitacora</returns>
-    public async Task<RespuestaColeccion<Bitacora>> Paginado(Paginado paginado, List<Condicion> condicion)
-      => await base.Obtener(paginado, condicion);
-
-    /// <summary>
-    /// Devuelve todos los registros de Bitacora incluyendo solo la columna
-    /// indicada
-    /// </summary>
-    /// <param name="columna">Nombre de la columna</param>
-    /// <returns>Coleccion de elementos</returns>
-    public async Task<RespuestaColeccion<IndiceValor>> ObtenerSoloUnaColumna(string columna)
-      => await base.Obtener(columna);
-
-    /// <summary>
-    /// Devuelve todos los registros de Bitacora dentro de un modelo de datos paginado
-    /// incluyrendo solo las columnas indicadas
-    /// </summary>
-    /// <param name="columnas">Nombres de columnas</param>
-    /// <param name="paginado">Solicitud de pagina</param>
-    /// <param name="condicion">Condiciones de busqueda</param>
-    /// <returns>Coleccion de elementos de Bitacora</returns>
-    public async Task<RespuestaColeccion<Bitacora>> PaginadoPorColumnas(string[] columnas, Paginado paginado, List<Condicion> condicion = null)
-      => await base.Obtener(columnas, paginado, condicion);
+      => await base.Obtener(id);    
 
     /// <summary>
     /// Guarda los cambios efectuados de la entidad
@@ -64,52 +34,7 @@ namespace Negocio.ProveedoresDeDatos
     /// </summary>
     /// <param name="modelo">Entidad</param>
     /// <returns>Respuesta basica</returns>
-    public async Task<RespuestaBasica> GuardarUno(Bitacora modelo)
-      => await base.Guardar(modelo);
-
-    /// <summary>
-    /// Guarda los cambios efectuados de las entidades
-    /// nuevos/actualizacion
-    /// </summary>
-    /// <param name="entidades">Entidades</param>
-    /// <returns>Coleccion con identificadores primarios</returns>
-    public async Task<RespuestaColeccion<long>> GuardarListado(List<Bitacora> entidades)
-      => await base.Guardar(entidades);
-
-    /// <summary>
-    /// Guarda los valores de una entidad asumiendo
-    /// que es un registro nuevo
-    /// </summary>
-    /// <param name="modelo">Entidad</param>
-    /// <returns>Identificador primario de la entidad insertada</returns>
-    public async Task<RespuestaModelo<long>> InsertarUno(Bitacora modelo)
-      => await base.Insertar(modelo);
-
-    /// <summary>
-    /// Guarda los valores de las entidades asumiendo
-    /// que son registros nuevos
-    /// </summary>
-    /// <param name="entidades">Entidades</param>
-    /// <returns>Coleccion de identificadores primarios de las entidades insertadas</returns>
-    public async Task<RespuestaColeccion<long>> InsertarListado(List<Bitacora> entidades)
-      => await base.Insertar(entidades);
-
-    /// <summary>
-    /// Guarda los valores de una entidad asumiendo
-    /// que es un registro existente
-    /// </summary>
-    /// <param name="modelo">Entidad</param>
-    /// <returns>Cantidad de filas afectadas</returns>
-    public async Task<RespuestaModelo<long>> ActualizarUno(Bitacora modelo)
-      => await base.Actualizar(modelo);
-
-    /// <summary>
-    /// Guarda los valores de las entidades asumiendo
-    /// que son registros existentes
-    /// </summary>
-    /// <param name="entidades">Entidades</param>
-    /// <returns>Cantidad de filas afectadas</returns>
-    public async Task<RespuestaModelo<long>> ActualizarListado(List<Bitacora> entidades)
-      => await base.Actualizar(entidades);
+    public async Task<RespuestaBasica> GuardarBitacora(Bitacora modelo)
+      => await base.Guardar(modelo);    
   }
 }
