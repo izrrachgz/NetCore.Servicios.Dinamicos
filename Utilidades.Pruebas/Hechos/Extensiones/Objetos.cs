2db157faf4a084f1a82152cad9f31d1962f47d2f
israel chavez <izrra.ch@icloud.com>
2020-03-29 14:33:18 -0700

    +Pruebas de extensiones de objetos

---
+++ Utilidades.Pruebas/Hechos/Extensiones/Objetos.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Utilidades.Extensiones;
+using Xunit;
+
+namespace Utilidades.Pruebas.Hechos.Extensiones
+{
+  /// <summary>
+  /// Pruebas positivas de objetos
+  /// </summary>
+  public class Objetos
+  {
+    public Objetos()
+    {
+
+    }
+
+    /// <summary>
+    /// Comprueba que el objeto indicado contenga una lista
+    /// de valores alfanumericos
+    /// </summary>
+    [Fact]
+    public void EsColeccionDeCaracteres()
+    {
+      object lista = new List<string>(2) { @"1", @"2" };
+      Assert.True(lista.EsColeccionDeCaracteres());
+    }
+
+    /// <summary>
+    /// Comprueba que el objeto indicado contenga una lista
+    /// de valores numericos
+    /// </summary>
+    [Fact]
+    public void EsColeccionNumerica()
+    {
+      object lista = new List<int>(2) { 1, 2 };
+      Assert.True(lista.EsColeccionNumerica());
+    }
+  }
+}
